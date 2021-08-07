4263a02097aafdc2c74e865ae71817d1c6fe337b
henrey22 <pedro.novaes@nave.org.br>
2021-08-07 19:08:17 -0300

    dia 3

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ for (const link of links) {
     })
 }
 
+//------Scroll-----//
 
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
@@ -26,3 +27,28 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+//------Swiper-----//
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  //------Scroll Reveal-----//
+
+  const scrollReveal = ScrollReveal({
+      origin: 'top',
+      distance: '30px',
+      duration: 700,
+      reset: true
+  })
+
+  scrollReveal.reveal(`#home .text, #home .img,
+                        #about .text, #about .img, 
+                        #services header, #services .card,
+                        #testimonials  header, #testimonials .testimonial
+                        #contact text, #contact .links`, {interval: 100})
